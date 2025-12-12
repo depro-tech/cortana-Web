1988fc42f1aa77b6bac9947ea607c0d7296b3724
depro-tech <karizedu159@gmail.com>
2025-12-12 21:47:58 +0300

    Update db.ts

--- CORTANA-WEB-2/server/db.ts
+++ CORTANA-WEB-2/server/db.ts
@@ -3,7 +3,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 import * as schema from "@shared/schema";
 
-if (!process.env.postgres://avnadmin:AVNS_-tuoyew5ufmdwFQkKU6@pg-1cd95cc0-karizedu159-776c.c.aivencloud.com:28013/defaultdb?sslmode=require) {
+if (!process.env.DATABASE_URL {
   throw new Error("DATABASE_URL environment variable is not set");
 }
 
