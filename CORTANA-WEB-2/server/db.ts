aefecb14c178cba0a9d8ae5b93890ccdf1deabab
depro-tech <karizedu159@gmail.com>
2025-12-12 22:04:33 +0300

    Update db.ts

--- CORTANA-WEB-2/server/db.ts
+++ CORTANA-WEB-2/server/db.ts
@@ -3,9 +3,9 @@ import pkg from "pg";
 const { Pool } = pkg;
 import * as schema from "@shared/schema";
 
-if (!process.env.DATABASE_URL (
+if (!process.env.DATABASE_URL {
   throw new Error("DATABASE_URL environment variable is not set");
-)
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
